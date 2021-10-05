9f17d1c43e38cd0f8823e3be9cc403ec690682ed
leonardobarone <leonardobarone26@gmail.com>
2021-10-05 19:52:55 +0200

    avanzamento

---
+++ src/components/Film.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="film">
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Film'
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/components/Films.vue
+++ src/components/Films.vue
@@ -1,19 +1,42 @@
 <template>
   <div class="films">
+      <ul>
+          <li v-for="(film, index) in films" :key="index">{{film.original_title}}</li>
+      </ul>
       <Film />
   </div>
 </template>
 
 <script>
 import Film from './Film.vue'
+import axios from 'axios';
 
 export default {
     name: 'Films',
     components: {
         Film
     },
+    data() {
+        return {
+            films: []
+        }
+    },
     props: {
         inputSearch: String
+    },
+    watch: {
+        prova() {
+            axios.get('https://api.themoviedb.org/3/search/movie', {
+                params: {
+                    api_key: '933f816273dcac2d9bb3de85d7f0a2c6',
+                    query: this.inputSearch,
+                    language: 'it-IT'
+                }
+            })
+            .then((response) => {
+                this.films = response.data.results
+            });
+        } 
     }
 }
 </script>
